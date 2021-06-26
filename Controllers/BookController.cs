d90599c9e4e0145052a42bb6fd01c3eff3d95346
Dat2296 <minhdatc3@gmail.com>
2021-06-26 21:55:25 +0700

    Initial commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,111 @@
+﻿using BookManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.ModelBinding;
+using System.Data;
+namespace BookManager.Controllers
+
+{
+    public class BookController : Controller
+    {
+        
+        BookManagerContext db = new BookManagerContext();
+        public ActionResult ListBook()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listbook = context.Book.ToList();
+            return View(listbook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Book.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create([Bind(Include = "ID,Author,Title,Description,Images,Price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Book.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("ListBook");
+            }
+
+            return View(book);
+        }
+        ///
+        [Authorize]
+        public ActionResult EditBook(int? id)
+        {
+           
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Book.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult EditBook([Bind(Include = "ID,Author,Title,Description,ImageCover,Price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListBook");
+            }
+            return View(book);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Book.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Book.Find(id);
+            db.Book.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("ListBook");
+        }
+    }
+}
+
+        // GET: Book
+    
